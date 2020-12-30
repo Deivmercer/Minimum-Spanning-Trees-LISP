a645a0d2344031a7443fa98eeeeba981c1c77473
Davide Costantini <dende306@gmail.com>
2020-12-30 14:42:59 +0100

    Sistemazioni varie alle funzioni per i grafi

--- minimum-spanning-trees.lisp
+++ minimum-spanning-trees.lisp
@@ -17,9 +17,11 @@
 (defparameter *previous* 
     (make-hash-table :test #'equal))
 
-;; Ritorna il graph-id stesso se questo grafo è già stato creato, oppure NILse no.  
+;; Ritorna il graph-id stesso se questo grafo è già stato creato, oppure NIL se no.  
 (defun is-graph (graph-id)
-    (gethash graph-id *graphs*))
+    (cond
+        ((gethash graph-id *graphs*)
+            graph-id)))
 
 ;; Genera un nuovo grafo e lo inserisce nel data base (ovvero nella hash-table) dei grafi.
 (defun new-graph (graph-id)
@@ -28,79 +30,116 @@
 
 ;; Rimuove l’intero grafo dal sistema
 (defun delete-graph (graph-id)
-    (remhash graph-id *graphs*))
+    (remhash graph-id *graphs*)
+    (maphash 
+        #'(lambda (&rest hash-entry) 
+            (cond 
+                ((string= graph-id (car (cdr (car hash-entry))))
+                    (remhash (car hash-entry) *vertices*))))
+        *vertices*)
+    (maphash 
+        #'(lambda (&rest hash-entry) 
+            (cond 
+                ((string= graph-id (car (cdr (car hash-entry))))
+                    (remhash (car hash-entry) *arcs*))))
+        *arcs*))
 
 ;; Aggiunge un nuovo vertice vertex-id al grafo graph-id.
 (defun new-vertex (graph-id vertex-id)
+    (cond 
+        ((not (gethash graph-id *graphs*))
+            (new-graph graph-id)))
     (setf 
         (gethash (list 'vertex graph-id vertex-id) *vertices*)
         (list 'vertex graph-id vertex-id)))
 
 ;; Ritorna una lista di vertici del grafo.
 (defun graph-vertices (graph-id)
+    (cond 
+        ((not (gethash graph-id *graphs*))
+            (error "Il grafo specificato non esiste")))
     (let ((vertices ()))
         (maphash 
             #'(lambda (&rest hash-entry) 
-                    (cond 
-                        ((string= graph-id (car (cdr (car hash-entry)))) 
-                            (push (car hash-entry) vertices))))
+                (cond 
+                    ((string= graph-id (car (cdr (car hash-entry))))
+                        (push (car hash-entry) vertices))))
             *vertices*)
         vertices))
 
 ;; Aggiunge un arco del grafo graph-id nella hash-table *arcs*.
 (defun new-arc (graph-id source-vertex-id dest-vertex-id &optional (weight 0))
+    (cond 
+        ((not (gethash (list 'vertex graph-id source-vertex-id) *vertices*))
+            (new-vertex graph-id source-vertex-id)))
+    (cond 
+        ((not (gethash (list 'vertex graph-id dest-vertex-id) *vertices*))
+            (new-vertex graph-id dest-vertex-id)))
     (setf 
         (gethash (list 'arc graph-id source-vertex-id dest-vertex-id weight) *arcs*)
         (list 'arc graph-id source-vertex-id dest-vertex-id weight)))
 
 ;; Ritorna una lista di tutti gli archi presenti in graph-id.
 (defun graph-arcs (graph-id)
+    (cond 
+        ((not (gethash graph-id *graphs*))
+            (error "Il grafo specificato non esiste")))
     (let ((arcs ()))
         (maphash 
             #'(lambda (&rest hash-entry) 
-                    (cond 
-                        ((string= graph-id (car (cdr (car hash-entry)))) 
-                            (push (car hash-entry) arcs))))
+                (cond 
+                    ((string= graph-id (car (cdr (car hash-entry))))
+                        (push (car hash-entry) arcs))))
             *arcs*)
         arcs))
 
 ;; Ritorna una lista contenente gli archi che portano ai vertici N immediatamente raggiungibili da vertex-id.
 (defun graph-vertex-neighbors (graph-id vertex-id)
+    (cond 
+        ((not (gethash graph-id *graphs*))
+            (error "Il grafo specificato non esiste."))
+        ((not (gethash (list 'vertex graph-id vertex-id) *vertices*))
+            (error "Il vertice specificato non esiste.")))
     (let ((neighbors ()))
-            (maphash 
-                #'(lambda (&rest hash-entry) 
+        (maphash 
+            #'(lambda (&rest hash-entry) 
+                (cond 
+                    ((string= graph-id (car (cdr (car hash-entry)))) 
                         (cond 
-                            ((string= graph-id (car (cdr (car hash-entry)))) 
-                                (cond 
-                                    ((string= vertex-id (car (cdr (cdr (car hash-entry))))) 
-                                        (push (car hash-entry) neighbors))
-                                    ((string= vertex-id (car (cdr (cdr (cdr (car hash-entry)))))) 
-                                        (push (car hash-entry) neighbors))))))
-                *arcs*)
-            neighbors))
+                            ((string= vertex-id (car (cdr (cdr (car hash-entry))))) 
+                                (push (car hash-entry) neighbors))
+                            ((string= vertex-id (car (cdr (cdr (cdr (car hash-entry)))))) 
+                                (push (car hash-entry) neighbors))))))
+            *arcs*)
+        neighbors))
 
 ;; Ritorna una lista contenente i vertici adiacenti a vertex-id.
 (defun graph-vertex-adjacent (graph-id vertex-id)
+    (cond 
+        ((not (gethash graph-id *graphs*))
+            (error "Il grafo specificato non esiste."))
+        ((not (gethash (list 'vertex graph-id vertex-id) *vertices*))
+            (error "Il vertice specificato non esiste.")))
     (let ((adjacent ()))
-            (maphash 
-                #'(lambda (&rest hash-entry) 
+        (maphash 
+            #'(lambda (&rest hash-entry) 
+                (cond 
+                    ((string= graph-id (car (cdr (car hash-entry)))) 
                         (cond 
-                            ((string= graph-id (car (cdr (car hash-entry)))) 
-                                (cond 
-                                    ((string= vertex-id (car (cdr (cdr (car hash-entry))))) 
-                                        (push 
-                                            (gethash 
-                                                (list 'vertex graph-id (car (cdr (cdr (cdr (car hash-entry)))))) 
-                                                *vertices*) 
-                                            adjacent))
-                                    ((string= vertex-id (car (cdr (cdr (cdr (car hash-entry)))))) 
-                                        (push 
-                                            (gethash 
-                                                (list 'vertex graph-id (car (cdr (cdr (car hash-entry))))) 
-                                                *vertices*) 
-                                            adjacent))))))
-                *arcs*)
-            adjacent))
+                            ((string= vertex-id (car (cdr (cdr (car hash-entry))))) 
+                                (push 
+                                    (gethash 
+                                        (list 'vertex graph-id (car (cdr (cdr (cdr (car hash-entry)))))) 
+                                        *vertices*) 
+                                    adjacent))
+                            ((string= vertex-id (car (cdr (cdr (cdr (car hash-entry)))))) 
+                                (push 
+                                    (gethash 
+                                        (list 'vertex graph-id (car (cdr (cdr (car hash-entry))))) 
+                                        *vertices*) 
+                                    adjacent))))))
+            *arcs*)
+        adjacent))
 
 ;; Stampa alla console dell’interprete una lista dei vertici e degli archi del grafo graph-id.
 (defun graph-print (graph-id)
