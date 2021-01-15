b7b0608bdee6f7b18c10556d81ed2242311794c2
Davide Costantini <dende306@gmail.com>
2021-01-15 20:02:09 +0100

    Correzioni ed ottimizzazioni varie

--- graph.lisp
+++ graph.lisp
@@ -2,7 +2,7 @@
 ;;;;
 ;;;; Autore: Davide Costantini
 ;;;;
-;;;; Libreria per la gestione di grafi diretti, connessi e con cicli.
+;;;; Libreria per la gestione di grafi.
 
 ;;; Tabella contenente le rappresentazioni dei grafi.
 (defparameter *graphs* (make-hash-table :test #'equal))
@@ -29,23 +29,33 @@ la rappresentazione del vertice. Se il grafo graph-id non esiste al momento
 della chiamata allora verrà creato."
   (cond ((not (is-graph graph-id))
          (new-graph graph-id)))
-  (setf 
-   (gethash (list graph-id vertex-id) *vertices*) 
-   (list 'vertex graph-id vertex-id)))
+  (setf (gethash (list graph-id vertex-id) *vertices*) 
+        (list 'vertex graph-id vertex-id)))
 
 (defun graph-vertices (graph-id)
   "Ritorna una lista contenente tutti i vertici relativi al grafo graph-id."
   (cond ((not (is-graph graph-id))
          (error "Il grafo specificato non esiste.")))
   (let ((vertices ()))
-    (maphash 
-     #'(lambda (key value) 
-         (cond ((string= graph-id (car key))
-                (push value vertices))))
-     *vertices*)
+    (maphash (lambda (key value) 
+               (cond ((string= graph-id (car key))
+                      (push value vertices))))
+             *vertices*)
     vertices))
 
-(defun new-arc (graph-id source-vertex-id dest-vertex-id &optional (weight 0))
+(defun arc-source (arc)
+  "Ritorna il vertice sorgente dell'arco arc."
+  (car (cdr (cdr arc))))
+
+(defun arc-destination (arc)
+  "Ritorna il vertice destinazione dell'arco arc."
+  (car (cdr (cdr (cdr arc)))))
+
+(defun arc-weight (arc)
+  "Ritorna il peso dell'arco arc."
+  (car (cdr (cdr (cdr (cdr arc))))))
+
+(defun new-arc (graph-id source-vertex-id dest-vertex-id &optional (weight 1))
   "Crea un nuovo arco per il grafo graph-id che va dal vertice source-vertex-id
 al vertice dest-vertex-id con peso weight e ritorna la rappresentazione
 dell'arco. Se i due vertici non esistono al momento della chiamata allora
@@ -54,94 +64,89 @@ verranno creati."
          (new-vertex graph-id source-vertex-id)))
   (cond ((not (gethash (list graph-id dest-vertex-id) *vertices*))
          (new-vertex graph-id dest-vertex-id)))
-  (cond
-   ((gethash (list graph-id source-vertex-id dest-vertex-id) *arcs*)
-    (remhash (list graph-id source-vertex-id dest-vertex-id) *arcs*))
-   ((gethash (list graph-id dest-vertex-id source-vertex-id) *arcs*)
-    (remhash (list graph-id dest-vertex-id source-vertex-id) *arcs*)))
-  (setf 
-   (gethash (list graph-id source-vertex-id dest-vertex-id) *arcs*)
-   (list 'arc graph-id source-vertex-id dest-vertex-id weight)))
+  (cond ((gethash (list graph-id source-vertex-id dest-vertex-id) *arcs*)
+         (remhash (list graph-id source-vertex-id dest-vertex-id) *arcs*))
+        ((gethash (list graph-id dest-vertex-id source-vertex-id) *arcs*)
+         (remhash (list graph-id dest-vertex-id source-vertex-id) *arcs*)))
+  (setf (gethash (list graph-id source-vertex-id dest-vertex-id) *arcs*)
+        (list 'arc graph-id source-vertex-id dest-vertex-id weight)))
 
 (defun graph-arcs (graph-id)
   "Ritorna una lista contenente tutti gli archi relativi al grafo graph-id."
   (cond ((not (is-graph graph-id))
          (error "Il grafo specificato non esiste.")))
   (let ((arcs ()))
-    (maphash 
-     #'(lambda (key value) 
-         (cond ((string= graph-id (car key))
-                (push value arcs))))
-     *arcs*)
+    (maphash (lambda (key value) 
+               (cond ((string= graph-id (car key))
+                      (push value arcs))))
+             *arcs*)
     arcs))
 
 (defun graph-vertex-neighbors (graph-id vertex-id)
   "Ritorna una lista contenente gli archi che portano ai vertici adiacenti a
 vertex-id."
-  (cond 
-   ((not (is-graph graph-id))
-    (error "Il grafo specificato non esiste."))
-   ((not (gethash (list graph-id vertex-id) *vertices*))
-    (error "Il vertice specificato non esiste.")))
+  (cond ((not (is-graph graph-id))
+         (error "Il grafo specificato non esiste."))
+        ((not (gethash (list graph-id vertex-id) *vertices*))
+         (error "Il vertice specificato non esiste.")))
   (let ((neighbors ()))
-    (maphash
-     #'(lambda (key value) 
-         (cond ((string= graph-id (car key))
-                (cond 
-                 ((numberp vertex-id)
-                  (cond
-                   ((= vertex-id (car (cdr key))) 
-                    (push value neighbors))
-                   ((= vertex-id (car (cdr (cdr key)))) 
-                    (push value neighbors))))
-                 (t (cond
-                     ((string= vertex-id (car (cdr key))) 
-                      (push value neighbors))
-                     ((string= vertex-id (car (cdr (cdr key)))) 
-                      (push value neighbors))))))))
-     *arcs*)
+    (maphash (lambda (key value) 
+               (cond ((string= graph-id (car key))
+                      (cond ((numberp vertex-id)
+                             (cond ((= vertex-id (arc-source value)) 
+                                    (push value neighbors))
+                                   ((= vertex-id (arc-destination value))
+                                    (push (list 'arc graph-id vertex-id 
+                                                (arc-source value)
+                                                (arc-weight value))
+                                          neighbors))))
+                            (t (cond ((string= vertex-id (arc-source value))
+                                      (push value neighbors))
+                                     ((string= vertex-id 
+                                               (arc-destination value))
+                                      (push (list 'arc graph-id vertex-id 
+                                                  (arc-source value) 
+                                                  (arc-weight value))
+                                            neighbors))))))))
+             *arcs*)
     neighbors))
 
 (defun graph-vertex-adjacent (graph-id vertex-id)
   "Ritorna una lista contenente i vertici adiacenti a vertex-id."
-  (cond 
-   ((not (is-graph graph-id))
-    (error "Il grafo specificato non esiste."))
-   ((not (gethash (list graph-id vertex-id) *vertices*))
-    (error "Il vertice specificato non esiste.")))
+  (cond ((not (is-graph graph-id))
+         (error "Il grafo specificato non esiste."))
+        ((not (gethash (list graph-id vertex-id) *vertices*))
+         (error "Il vertice specificato non esiste.")))
   (let ((adjacent ()))
-    (maphash 
-     #'(lambda (&rest hash-entry) 
-         (cond ((string= graph-id (car (car hash-entry)))
-                (cond 
-                 ((numberp vertex-id)
-                  (cond 
-                   ((= vertex-id (car (cdr (car hash-entry))))
-                    (push 
-                     (gethash 
-                      (list graph-id (car (cdr (cdr (car hash-entry)))))
-                      *vertices*) 
-                     adjacent))
-                   ((= vertex-id (car (cdr (cdr (car hash-entry)))))
-                    (push 
-                     (gethash 
-                      (list graph-id (car (cdr (car hash-entry))))
-                      *vertices*) 
-                     adjacent))))
-                 (t (cond 
-                     ((string= vertex-id (car (cdr (car hash-entry))))
-                      (push 
-                       (gethash 
-                        (list graph-id (car (cdr (cdr (car hash-entry)))))
-                        *vertices*) 
-                       adjacent))
-                     ((string= vertex-id (car (cdr (cdr (car hash-entry)))))
-                      (push 
-                       (gethash 
-                        (list graph-id (car (cdr (car hash-entry))))
-                        *vertices*) 
-                       adjacent))))))))
-     *arcs*)
+    (maphash (lambda (key value) 
+               (cond ((string= graph-id (car key))
+                      (cond ((numberp vertex-id)
+                             (cond ((= vertex-id (arc-source value))
+                                    (push 
+                                     (gethash (list graph-id 
+                                                    (arc-destination value))
+                                              *vertices*) 
+                                     adjacent))
+                                   ((= vertex-id (arc-destination value))))
+                             (push 
+                              (gethash (list graph-id 
+                                             (arc-source value))
+                                       *vertices*) 
+                              adjacent))))
+                     (t (cond ((string= vertex-id (arc-source value))
+                               (push 
+                                (gethash (list graph-id 
+                                               (arc-destination value))
+                                         *vertices*) 
+                                adjacent))
+                              ((string= vertex-id 
+                                        (arc-destination value))
+                               (push 
+                                (gethash (list graph-id 
+                                               (arc-source value))
+                                         *vertices*) 
+                                adjacent))))))
+             *arcs*)
     adjacent))
 
 (defun graph-print (graph-id)
--- heap.lisp
+++ heap.lisp
@@ -15,11 +15,9 @@ ritorna la rappresentazione dell'heap."
        (gethash heap-id *heaps*)
        (list 'heap heap-id 0 (make-array capacity)))))
 
-;; TODO: Forse c'è da implementare anche heap-id? (e nel caso heap-size ed 
-;;       heap-actual-heap potrebbbero essere sbagliate,
-;;       in quanto dovrebbero prendere non l'id ma heap-rep)
-;;       https://elearning.unimib.it/mod/forum/discuss.php?d=148950#p252156
-;; (defun heap-id (?))
+(defun heap-id (heap-id)
+  "Ritorna T se esiste un heap di nome heap-id, NIL altrimenti."
+  (cond ((gethash heap-id *heaps*) t)))
 
 (defun heap-size (heap-id)
   "Ritorna la dimensione effettiva dell'heap heap-id."
@@ -35,11 +33,9 @@ ritorna la rappresentazione dell'heap."
 
 (defun heap-empty (heap-id)
   "Ritorna T se l'heap heap-id è vuoto, NIL altrimenti."
-  (cond 
-   ((not (gethash heap-id *heaps*))
-    (error "Lo heap specificato non esiste.")))
-  (cond
-   ((= (heap-size heap-id) 0))))
+  (cond ((not (gethash heap-id *heaps*))
+         (error "Lo heap specificato non esiste.")))
+  (cond ((= (heap-size heap-id) 0))))
 
 (defun heap-not-empty (heap-id)
   "Ritorna T se l'heap heap-id non è vuoto, NIL altrimenti."
@@ -50,78 +46,69 @@ ritorna la rappresentazione dell'heap."
 
 (defun heap-head (heap-id)
   "Ritorna l'elemento in testa all'heap."
-  (cond 
-   ((not (gethash heap-id *heaps*))
-    (error "Lo heap specificato non esiste."))
-   ((heap-empty heap-id)
-    (error "Lo heap specificato e' vuoto.")))
+  (cond ((not (gethash heap-id *heaps*))
+         (error "Lo heap specificato non esiste."))
+        ((heap-empty heap-id)
+         (error "Lo heap specificato e' vuoto.")))
   (aref (heap-actual-heap heap-id) 0))
 
 (defun heapify-insert (heap-id index)
   "Ritorna T quando l'heap soddisfa la proprietà di heap."
-  (cond
-   ((= index 0) t)
-   (t (let ((current (aref (heap-actual-heap heap-id) index))
-            (parent (aref (heap-actual-heap heap-id) (floor (/ index 2)))))
-        (cond ((< (car current) (car parent)) 
-               (setf (aref (heap-actual-heap heap-id) index) parent)
-               (setf 
-                (aref (heap-actual-heap heap-id) (floor (/ index 2))) 
-                current)
-               (heapify-insert heap-id (floor (/ index 2))))))
-      t)))
- 
+  (let ((heap (heap-actual-heap heap-id)))
+    (cond ((= index 0) t)
+          (t (let ((current (aref heap index))
+                   (parent (aref heap (floor (/ index 2)))))
+               (cond ((< (car current) (car parent)) 
+                      (setf (aref heap index) parent)
+                      (setf (aref heap (floor (/ index 2))) current)
+                      (heapify-insert heap-id (floor (/ index 2))))))
+             t))))
+
 (defun heap-insert (heap-id K V)
   "Ritorna T quando è stato possibile inserire l'elemento (K V) nell'heap
 heap-id e l'heap soddisfa la proprietà di heap."
-  (cond 
-   ((not (gethash heap-id *heaps*))
-    (new-heap heap-id))
-   ((= (heap-size heap-id) (length (heap-actual-heap heap-id)))
-    (setf
-     (gethash heap-id *heaps*)
-     (list 'heap heap-id 
-           (heap-size heap-id)
-           (adjust-array 
-            (heap-actual-heap heap-id) 
-            (+ (heap-size heap-id) 1))))))
-  (setf (aref (heap-actual-heap heap-id) (heap-size heap-id)) (list K V))
-  (setf
-   (gethash heap-id *heaps*)
-   (list 'heap heap-id (+ (heap-size heap-id) 1) (heap-actual-heap heap-id)))
-  (heapify-insert heap-id (- (heap-size heap-id) 1)))
+  (cond ((not (gethash heap-id *heaps*))
+         (new-heap heap-id)))
+  (let ((heap (heap-actual-heap heap-id))
+        (size (heap-size heap-id)))
+    (cond ((= size (length heap))
+           (setf (gethash heap-id *heaps*)
+                 (list 'heap heap-id size (adjust-array heap (+ size 1))))))
+    (setf (aref heap size) (list K V))
+    (setf (gethash heap-id *heaps*)
+          (list 'heap heap-id (+ size 1) heap))
+    (heapify-insert heap-id size)))
 
 (defun smallest (heap index-x index-y size)
   "Ritorna l'elemento con chiave più piccola tra quello in posizione index-x
 e quello in posizione index-y."
-  (cond 
-   ((< index-x size)
-    (cond
-     ((<= (car (aref heap index-x)) (car (aref heap index-y)))
-      index-x)
-     (t index-y)))
-   (t index-y)))
+  (cond ((< index-x size)
+         (cond ((<= (car (aref heap index-x)) (car (aref heap index-y)))
+                index-x)
+               (t index-y)))
+        (t index-y)))
 
 (defun swap (heap index to-swap)
   "Scambia i valori degli elementi in posizione index e to-swap e ritorna
 to-swap"
-  (cond
-   ((/= index to-swap)
-    (let ((x (aref heap index))
-          (y (aref heap to-swap)))
-      (setf (aref heap index) y)
-      (setf (aref heap to-swap) x))
-    to-swap)
-   (t -1)))
+  (cond ((/= index to-swap)
+         (let ((x (aref heap index))
+               (y (aref heap to-swap)))
+           (setf (aref heap index) y)
+           (setf (aref heap to-swap) x))
+         to-swap)
+        (t -1)))
 
 (defun heapify (heap index size)
   "Ritorna T quando l'heap soddisfa la proprietà di heap."
   (cond ((/= index -1) 
          (heapify heap 
                   (swap heap index 
-                        (smallest heap (- (+ (* (+ index 1) 2) 1) 1)
-                                  (smallest heap (- (* (+ index 1) 2) 1) index 
-                                            size) 
+                        (smallest heap 
+                                  (- (+ (* (+ index 1) 2) 1) 1)
+                                  (smallest heap 
+                                            (- (* (+ index 1) 2) 1) 
+                                            index size) 
                                   size))
                   size))))
 
@@ -138,71 +125,47 @@ e l'heap soddisfa ancora la proprietà di heap."
            (heapify heap 0 (- size 1))))
     current-head))
 
-(defun mapheap (func heap heap-size &optional (index 1))
+(defun mapheap (func heap size &optional (index 1))
   "Applica la funzione func ad ogni elemento dell'heap e ritorna NIL."
-  (cond ((< (- index 1) heap-size)
+  (cond ((< (- index 1) size)
          (funcall func (aref heap (- index 1)) (- index 1))
-         (mapheap func heap heap-size (* index 2))
-         (mapheap func heap heap-size (+ (* index 2) 1)))))
+         (mapheap func heap size (* index 2))
+         (mapheap func heap size (+ (* index 2) 1)))))
+
+(defun modify-entry (heapid heap size new-key old-key value &optional (index 0))
+  "Modifica il primo elemento la cui chiave è old-key ed il valore è value
+impostando new-key come chiave."
+  (cond ((< index size)
+         (cond ((equal (aref heap index) (list old-key value))
+                (setf (aref heap index) 
+                      (list new-key value))
+                (cond ((< new-key old-key)
+                       (heapify-insert heapid index) t)
+                      ((> new-key old-key)
+                       (heapify heap index size) t)))     
+               (t (modify-entry heapid heap size new-key old-key value 
+                                (+ index 1)))))))
 
 (defun heap-modify-key (heap-id new-key old-key V)
   "Ritorna T se è stato possibile modificare la chiave dell'elemento dell'heap
 heap-id con chiave old-key e valore V in new-key e l'heap soddisfa ancora
 la proprietà di heap."
-  (cond
-   ((not (gethash heap-id *heaps*))
-    (error "Lo heap specificato non esiste."))
-   ((= new-key old-key) t)
-   (t (mapheap 
-       #'(lambda (heap-entry index) 
-           (cond ((= (car heap-entry) old-key)
-                  (cond
-                   ((numberp V) 
-                    (cond ((= (car (cdr heap-entry)) V)
-                           (setf 
-                            (aref (heap-actual-heap heap-id) index) 
-                            (list new-key V))
-                           (cond
-                            ((< new-key old-key)
-                             (heapify-insert heap-id index))
-                            ((> new-key old-key)
-                             (heapify (heap-actual-heap heap-id)
-                                      index (heap-size heap-id)))))))
-                   (t (cond ((string= (car (cdr heap-entry)) V)
-                             (setf 
-                              (aref (heap-actual-heap heap-id) index) 
-                              (list new-key V))
-                             (cond
-                              ((< new-key old-key)
-                               (heapify-insert heap-id index))
-                              ((> new-key old-key)
-                               (heapify (heap-actual-heap heap-id)
-                                        index (heap-size heap-id)))))))))))
-       (heap-actual-heap heap-id)
-       (heap-size heap-id)))))
+  (cond ((not (gethash heap-id *heaps*))
+         (error "Lo heap specificato non esiste."))
+        ((= new-key old-key) t)
+        (t (modify-entry heap-id 
+                         (heap-actual-heap heap-id) 
+                         (heap-size heap-id) 
+                         new-key old-key V))))
 
 (defun heap-print (heap-id)
   "Stampa una lista contenente gli elementi dell'heap heap-id e ritorna T."
   (cond ((not (gethash heap-id *heaps*))
          (error "Lo heap specificato non esiste.")))
   (let ((heap-entries ()))
-    (mapheap 
-     #'(lambda (heap-entry index) 
-         (push (cons index heap-entry) heap-entries))
-     (heap-actual-heap heap-id)
-     (heap-size heap-id))
-    (print (reverse heap-entries)))
-  t)
-
-(defun heap-current-key (heap size value &optional (index 0))
-  "Ritorna la chiave dell'elemento con valore V all'interno dell'heap"
-  (cond ((< index size)
-         (cond 
-          ((numberp value)
-           (cond 
-            ((= value (car (cdr (aref heap index)))) (car (aref heap index)))
-            (t (heap-current-key heap size value (+ index 1)))))
-          (t (cond
-              ((string= value (car (cdr (aref heap index)))) 
-               (car (aref heap index)))
-              (t (heap-current-key heap size value (+ index 1)))))))))
+    (mapheap (lambda (heap-entry index) 
+               (push (cons index heap-entry) heap-entries))
+             (heap-actual-heap heap-id)
+             (heap-size heap-id))
+    (print (reverse heap-entries))
+    t))
